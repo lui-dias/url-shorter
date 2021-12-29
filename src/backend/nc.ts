b3769b7cf616474ad8f6bd41940b312ca8d8e475
ellandor <66072698+ellandor@users.noreply.github.com>
2021-12-29 09:03:49 -0300

    fix: cors error in client request

--- src/backend/nc.ts
+++ src/backend/nc.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
 import { onError } from './errors'
+import cors from 'cors'
 
-export const ncBase = () => nc<NextApiRequest, NextApiResponse>({
-    onNoMatch: (_req, res) => res.status(405).json({ error: 'Method Not Allowed' }),
-    onError
-})
+export const ncBase = () =>
+    nc<NextApiRequest, NextApiResponse>({
+        onNoMatch: (_req, res) =>
+            res.status(405).json({ error: 'Method Not Allowed' }),
+        onError,
+    }).use(cors())
